2fe5a69824447458f9a5f86862b61536e0df6c1e
rje <me@rjevans.net>
2010-07-30 15:41:27 -0700

    Adding a missed "]" in the project.clj

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A simple clojure based blogging platform"
   :dependencies [[org.clojure/clojure "1.2.0-RC1"]
                  [org.clojure/clojure-contrib "1.2.0-RC1"]
-                 [org.clojars.rje/aleph "0.1.0-SNAPSHOT"]
+                 [org.clojars.rje/aleph "0.1.0-SNAPSHOT"]]
   :main kraken.core)
