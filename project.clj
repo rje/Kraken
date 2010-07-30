2d65033c12fa3af978b6a70ea1f8c891a6ddd846
rje <me@rjevans.net>
2010-07-30 15:38:06 -0700

    Initial commit of kraken, just text right now, code coming later

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject kraken "1.0.0-SNAPSHOT"
+  :description "A simple clojure based blogging platform"
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]
+                 [org.clojars.rje/aleph "0.1.0-SNAPSHOT"]
+  :main kraken.core)
---
+++ test/kraken/test/core.clj
@@ -0,0 +1,6 @@
+(ns kraken.test.core
+  (:use [kraken.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
