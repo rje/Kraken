fe9d6a19177ba138c7820059f18aedaf5e4ab0be
rje <me@rjevans.net>
2010-07-30 18:03:10 -0700

    whoops, forgot to checkin changes to core

--- src/kraken/core.clj
+++ src/kraken/core.clj
@@ -1,1 +1,7 @@
-(ns kraken.core)
+(ns kraken.core
+  (:gen-class)
+  (:require [aleph.http]
+            [kraken.routing :as routing]))
+
+(defn -main [& args]
+  (aleph.http/start-http-server routing/route {:port 8080}))
